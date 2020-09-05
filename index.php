32147f299489d409399e490ef077ca7a5199d1da
jekjekdagreyt <60175144+jekjekdagreyt@users.noreply.github.com>
2020-09-05 20:24:47 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,74 @@
+<html>
+	<head>	
+		<link rel="stylesheet" href="style.css">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+	</head>
+	<body>
+		<div class = "main">
+			<div class = "Upper">
+				<div id = "navigation">
+					<div class = "nleft">
+						<a href = "index.html" class = "asulat">Home</a>
+					</div>
+					<div class = "ncenter">
+						<div class = "bawbaw"></div>
+						<div class = "bawbaw"></div>
+						<div class = "bawbawx"></div>
+					</div>
+					<div class = "nright">
+						<a href = "index.html" class = "asulat">About me</a>
+					</div>
+				</div>
+			</div>	
+			<div class = "Body">
+				<div class = "slidershow middle">
+					<div class = "slides">
+						<input type = "radio" name = "r" id= "r1" checked>
+						<input type = "radio" name = "r" id= "r2">
+						<input type = "radio" name = "r" id= "r3">
+						<input type = "radio" name = "r" id= "r4">
+						<input type = "radio" name = "r" id= "r5">
+						
+						<div class = "slide s1">
+							<img src = "1.jpg" alt = "">
+						</div>
+						<div class = "slide">
+							<img src = "2.jpg" alt = "">
+						</div>
+						<div class = "slide">
+							<img src = "3.jpg" alt = "">
+						</div>
+						<div class = "slide">
+							<img src = "4.jpg" alt = "">
+						</div>
+						<div class = "slide">
+							<img src = "5.jpg" alt = "">
+						</div>
+					</div>	
+						<div class = "navb">
+							<label for= "r1" class="bar"></label>
+							<label for= "r2" class="bar"></label>
+							<label for= "r3" class="bar"></label>
+							<label for= "r4" class="bar"></label>
+							<label for= "r5" class="bar"></label>
+						</div>
+				</div>
+			</div>
+			<div class = "idalum">
+				<div class = "pargr">
+					<p class = "x">
+						Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
+						sed do eiusmod tempor incididunt
+						ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+						quis nostrud exercitation ullamco laboris nisi 
+						ut aliquip ex ea commodo consequat. Duis aute irure 
+						dolor in reprehenderit in voluptate 
+						velit esse cillum dolore eu fugiat nulla pariatur. 
+						Excepteur sint occaecat cupidatat non proident, 
+						sunt in culpa qui officia deserunt mollit anim id est laborum.
+					</p>
+				</div>
+			</div>
+		</div>
+	</body>
+</html>
